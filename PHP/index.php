11de0dd279c543b2b35509dc3c107254398fea5f
alexa <alexa@LAPTOP-TRE2VNJO>
2020-09-14 20:18:27 -0400

    Merge origin/master
    Conflicts:
    	css/stylesheet.css
    	nbproject/project.properties
    	nbproject/project.xml

--- PHP/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-     $pagecontents = file_get_contents("index.html");
-        echo str_replace("Banana", "Pineapple", $pagecontents);
-
-?>
--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,13 @@
  */
 
 
+document.addEventListener("orientationchange", function(event){
+    switch(window.orientation) 
+    {  
+        case -90: case 90:
+            /* Device is in landscape mode */
+            break; 
+        default:
+            /* Device is in portrait mode */
+    }
+});
